378f27f4f6aebbf1d3d93652c9a4e4b9ed205df4
Alvaro Guandique <alvaro_guandique@hotmail.com>
2024-06-03 11:37:08 -0600

    servicio para usuario

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, computed, signal } from '@angular/core';
+import { Account } from '../../types/account/account.type';
+import { Pokemon } from '../../types/pokemon/pokemon.type';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  // If we were using a real session case, it would be better to use rxjs(BehaviorSubject | Observable) to validate the 2 session states (user | null)
+  // private account = new BehaviorSubject<any | null | undefined>(null)
+  // public account$ = this.account.pipe(
+  //   distinctUntilChanged()
+  // )
+
+  private account = signal<Account | null>(null)
+  public accountPublic = computed(() => {
+    return this.account()
+  })
+
+  private chosenPokemons = signal<Pokemon[]>([])
+  public chosenPokemonsPublic = computed(() => {
+    return this.chosenPokemons()
+  })
+
+  constructor() {
+    const account = localStorage.getItem('pokeAppAccount')
+    if (account) {
+      const accountPrepared = JSON.parse(account)
+      this.account.set(accountPrepared)
+    }
+   }
+
+  setAccountData = (value: Account) => {
+    this.account.set(value)
+    localStorage.setItem('pokeAppAccount', JSON.stringify(value))
+  }
+
+  setPokemons = (pokemons: Pokemon[]) => {
+    this.chosenPokemons.set(pokemons)
+  }
+
+}
