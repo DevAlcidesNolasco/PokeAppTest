44e39d0e4f1b8a46b5fcc5a4d80fbe8f7307feb3
Alvaro Guandique <alvaro_guandique@hotmail.com>
2024-06-03 11:35:39 -0600

    rutas del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { hasUserGuard } from './guards/hasUser/has-user.guard';
+import { hasPokemonsGuard } from './guards/hasPokemons/has-pokemons.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'configurator',
+    loadComponent: () => import('./components/configurator/configurator.component').then((m) => m.ConfiguratorComponent),
+  },
+  {
+    path: 'pokemon-selector',
+    loadComponent: () => import('./components/configurator/components/pokemon-selector/pokemon-selector.component').then((m) => m.PokemonSelectorComponent),
+    canActivate: [hasUserGuard]
+  },
+  {
+    path: 'list',
+    loadComponent: () => import('./components/list/list.component').then((m) => m.ListComponent),
+    canActivate: [hasUserGuard, hasPokemonsGuard]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'list'
+  }
+];
